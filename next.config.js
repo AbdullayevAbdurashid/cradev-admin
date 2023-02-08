44dae460df2ff309dbdbb42909e48485f6121873
Abdullaev Abdurashid <abdulllayevabdurashid@gmail.com>
2023-02-08 18:54:42 +0500

    first commit

---
+++ next.config.js
@@ -0,0 +1,39 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+const { i18n } = require('./next-i18next.config');
+
+const nextConfig = {
+  reactStrictMode: true,
+  i18n,
+  pwa: {
+    disable: process.env.NODE_ENV === 'development',
+    dest: 'public',
+    runtimeCaching,
+  },
+  images: {
+    domains: [
+      'pickbazarlaravel.s3.ap-southeast-1.amazonaws.com',
+      "api.cradev.org",
+      "cradev.org",
+      'via.placeholder.com',
+      'res.cloudinary.com',
+      's3.amazonaws.com',
+      '127.0.0.1',
+      'localhost',
+      'picsum.photos',
+      'pixarlaravel.s3.ap-southeast-1.amazonaws.com',
+      'lh3.googleusercontent.com',
+    ],
+  },
+  ...(process.env.APPLICATION_MODE === 'production' && {
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+  }),
+};
+
+module.exports = withPWA(nextConfig);
